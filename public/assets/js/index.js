b7b59f54c6e30a1ab7f24062be39d7b9731d676d
Andre Lorente <andre.lorente83@gmail.com>
2022-08-10 13:30:17 -0300

    Submit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,7 @@
-function retornaLogin() {
+let formularioHTML = document.getElementById("formulario");
+
+formularioHTML.addEventListener("submit", (evento) => {
+  evento.preventDefault();
   alert("Conta Cadastrada com Sucesso");
   window.location.href = "index.html";
-}
+});
