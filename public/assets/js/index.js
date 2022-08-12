6be1f75736cbc2e36e4cdda7b4e67ec24da42039
Andre Lorente <andre.lorente83@gmail.com>
2022-08-12 19:54:26 -0300

    Iniciando Validacao Login

---
+++ public/assets/js/index.js
@@ -0,0 +1,13 @@
+let emailValidacao = document.getElementById("emailUsuario");
+let senhaValidacao = document.getElementById("senhaUsuario");
+
+let usuarioLogado = JSON.parse(localStorage.getItem("usuarios"));
+console.log(usuarioLogado);
+
+let validarSenha = usuarioLogado.some(
+  (usuarioValidacao) =>
+    usuarioValidacao.senha === senhaValidacao.value ||
+    usuarioValidacao.email === emailValidacao.value
+);
+console.log(validarSenha);
+console.log(validarEmail);
