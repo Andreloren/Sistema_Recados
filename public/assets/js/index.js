64cd75647d4ccef41a7c201f88b963726143f29b
Andre Lorente <andre.lorente83@gmail.com>
2022-08-13 08:40:41 -0300

    VAlidacoes Login

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,17 +1,46 @@
-let emailValidacao = document.getElementById("emailUsuario");
-let senhaValidacao = document.getElementById("senhaUsuario");
 let botaoLogin = document.getElementById("botao1");
-
-let usuarioLogin = JSON.parse(localStorage.getItem("usuarios"));
-console.log(usuarioLogin);
 
 botaoLogin.addEventListener("click", logarUsuario);
 
 function logarUsuario() {
+  let usuarioLogin = JSON.parse(localStorage.getItem("usuarios"));
+  console.log(usuarioLogin);
+
+  let emailValidacao = document.getElementById("emailUsuario").value;
+  let senhaValidacao = document.getElementById("senhaUsuario").value;
+
+  console.log(emailValidacao);
+  console.log(senhaValidacao);
+
+  if (!emailValidacao || !senhaValidacao) {
+    alert("erro. campos em branco");
+    resetLogin();
+    return;
+  }
+
   let validarUsuario = usuarioLogin.find(
     (usuarioValidacao) =>
-      usuarioValidacao.senha === senhaValidacao.value &&
-      usuarioValidacao.email === emailValidacao.value
+      usuarioValidacao.senha === senhaValidacao &&
+      usuarioValidacao.email === emailValidacao
   );
   console.log(validarUsuario);
+
+  if (validarUsuario === undefined) {
+    alert("Usuário ou senha inexistente.");
+    resetLogin();
+    return;
+  }
+
+  let usuarioLogado = validarUsuario;
+
+  localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
+}
+
+function resetLogin() {
+  document.getElementById("emailUsuario").value = "";
+  document.getElementById("senhaUsuario").value = "";
+}
+
+function retornaCadastro() {
+  window.location.href = "contanova.html";
 }
