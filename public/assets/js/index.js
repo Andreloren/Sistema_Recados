fd05ff0010d2adbb34cac9653254294cde881580
Andre Lorente <andre.lorente83@gmail.com>
2022-08-12 21:13:05 -0300

    id

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,6 @@
 let emailValidacao = document.getElementById("emailUsuario");
 let senhaValidacao = document.getElementById("senhaUsuario");
+let botaoLogin = document.getElementById("botao1");
 
 let usuarioLogado = JSON.parse(localStorage.getItem("usuarios"));
 console.log(usuarioLogado);
