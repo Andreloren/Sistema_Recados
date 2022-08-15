8baf2e6dfb7715f929ecfe47341df8028c07e1ca
Andre Lorente <andre.lorente83@gmail.com>
2022-08-15 13:33:17 -0300

    Recados

--- public/assets/js/recados.js
+++ public/assets/js/recados.js
@@ -15,3 +15,32 @@ botaoSair.addEventListener("click", () => {
 function sair() {
   return (window.location.href = "index.html");
 }
+let botaoSalvar = document.getElementById("salvar");
+
+let emailUsuario = document.getElementById("userEmail");
+emailUsuario.innerHTML = usuario.email;
+
+let descricaoHTML = document.getElementById("descricao");
+let detalhamentoHTML = document.getElementById("detalhamento");
+
+botaoSalvar.addEventListener("click", salvarMensagens);
+
+function salvarMensagens() {
+  let insereRecado = {
+    email: usuario.email,
+    identificador: usuario.identificador,
+    mensagens: [
+      {
+        descricao: descricaoHTML.value,
+        detalhamento: detalhamentoHTML.value,
+      },
+    ],
+    senha: usuario.senha,
+  };
+
+  console.log(descricaoHTML);
+  console.log(detalhamentoHTML);
+  console.log(insereRecado);
+
+  localStorage.setItem("usuarios", JSON.stringify(insereRecado));
+}
